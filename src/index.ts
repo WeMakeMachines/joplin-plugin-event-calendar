1fb4228634059a537014d53c35db78f0ccc895cf
Franco Speziali <franco@wemakemachines.com>
2022-03-17 15:54:24 +0100

    Updated plugin integration with Joplin

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import joplin from 'api';
+import joplin from "api";
+import { ContentScriptType } from "api/types";
 
 joplin.plugins.register({
-	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
+	onStart: async function () {
+		await joplin.contentScripts.register(
+			ContentScriptType.MarkdownItPlugin,
+			"joplin-plugin-event-calendar",
+			"./joplin-plugin-event-calendar.js"
+		);
 	},
 });
